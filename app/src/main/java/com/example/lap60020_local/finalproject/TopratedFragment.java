f12e7ede433c78528614f9a19ac501a4de7dbe74
tran chi khang <tranchikhangUS1996@gmail.com>
2018-06-29 15:16:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lap60020_local/finalproject/TopratedFragment.java
@@ -0,0 +1,29 @@
+package com.example.lap60020_local.finalproject;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TopratedFragment extends Fragment {
+
+
+    public TopratedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_toprated, container, false);
+    }
+
+}
